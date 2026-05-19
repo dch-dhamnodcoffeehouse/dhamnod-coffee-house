6001873210484491b936e2d06ba94a00b68e53d3
Dhamnod coffee house <coffeehousedhamnod@gmail.com>
2026-05-19 20:19:44 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,47 @@
-const CACHE_NAME = "dch-admin-v1";
+const CACHE_NAME = "dch-admin-v2";
 
 const urlsToCache = [
-  "/",
-  "/admin.html",
-  "/manifest.json"
+  "./",
+  "./admin.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+
+self.skipWaiting();
+
+event.waitUntil(
+caches.open(CACHE_NAME).then((cache) => {
+return cache.addAll(urlsToCache);
+})
+);
+
+});
+
+self.addEventListener("activate", (event) => {
+
+event.waitUntil(
+caches.keys().then((cacheNames) => {
+return Promise.all(
+cacheNames.map((cache) => {
+if (cache !== CACHE_NAME) {
+return caches.delete(cache);
+}
+})
+);
+})
+);
+
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+
+event.respondWith(
+caches.match(event.request).then((response) => {
+return response || fetch(event.request);
+})
+);
+
 });
