f2e64c22ae85911c19a28852300357bba4475af8
Dhamnod coffee house <coffeehousedhamnod@gmail.com>
2026-05-20 11:29:24 +0530

    Implement caching and fetch handling in service worker
    Add caching for specified URLs in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,34 @@
-self.addEventListener('install',event=>{
-console.log('Service Worker Installed');
+const CACHE_NAME = "dch-cache-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./admin.html",
+  "./analytics.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./alarm.ogg"
+];
+
+self.addEventListener("install", event => {
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urlsToCache))
+
+);
+
 });
 
-self.addEventListener('fetch',event=>{});
+self.addEventListener("fetch", event => {
+
+event.respondWith(
+
+caches.match(event.request)
+.then(response => response || fetch(event.request))
+
+);
+
+});
