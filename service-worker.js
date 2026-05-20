6c89f640df716619db4cf3a35f2d63081af504ea
Dhamnod coffee house <coffeehousedhamnod@gmail.com>
2026-05-20 10:52:50 +0530

    Update service-worker.js
    restored

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,5 @@
-const CACHE_NAME = "dch-admin-v2";
-
-const urlsToCache = [
-  "./",
-  "./admin.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", (event) => {
-
-self.skipWaiting();
-
-event.waitUntil(
-caches.open(CACHE_NAME).then((cache) => {
-return cache.addAll(urlsToCache);
-})
-);
-
+self.addEventListener('install',event=>{
+console.log('Service Worker Installed');
 });
 
-self.addEventListener("activate", (event) => {
-
-event.waitUntil(
-caches.keys().then((cacheNames) => {
-return Promise.all(
-cacheNames.map((cache) => {
-if (cache !== CACHE_NAME) {
-return caches.delete(cache);
-}
-})
-);
-})
-);
-
-});
-
-self.addEventListener("fetch", (event) => {
-
-event.respondWith(
-caches.match(event.request).then((response) => {
-return response || fetch(event.request);
-})
-);
-
-});
+self.addEventListener('fetch',event=>{});
