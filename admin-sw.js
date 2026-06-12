1d42059d578d4be6c747573c0e65589d30a95213
Dhamnod coffee house <coffeehousedhamnod@gmail.com>
2026-06-12 17:57:03 +0530

    Create admin-sw.js

---
+++ admin-sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "dch-admin-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./analytics.html"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  );
+
+});
