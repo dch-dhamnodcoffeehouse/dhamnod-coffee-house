bfdc91432f6fceffd2ccb03b38bd8334a281e56e
Dhamnod coffee house <coffeehousedhamnod@gmail.com>
2026-06-12 17:39:01 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dch-cache-v3";
+const CACHE_NAME = "dch-cache-v1";
 
 const urlsToCache = [
   "./",
@@ -9,19 +9,40 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
 
   event.waitUntil(
     caches.open(CACHE_NAME)
-    .then(cache => cache.addAll(urlsToCache))
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
 
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-
   event.respondWith(
-    caches.match(event.request)
-    .then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
-
 });
